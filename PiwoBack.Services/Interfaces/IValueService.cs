fd94a7ee79ed070a2dd3e54e92da2d6cae6ecc4f
BartusZak <b.ploszynski@gmail.com>
2018-04-14 01:37:23 +0200

    Add project files.

---
+++ PiwoBack.Services/Interfaces/IValueService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PiwoBack.Data.Models;
+
+namespace PiwoBack.Services.Interfaces
+{
+    public interface IValueService
+    {
+        IEnumerable<Value> GetAll();
+        Value GetValue(int id);
+    }
+}
